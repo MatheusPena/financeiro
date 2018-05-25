7ad9f37808e07f3d7071f7a7ed6b03f944151cd8
ayrtonsrego <ayrtonsrego@gmail.com>
2018-05-25 08:01:34 -0300

    Criação da classe que faz conexão com um banco de dados qualquer. Criação de duas classes no pacote domain só para testes. Por fim, criação do ManagedBean.

---
+++ financeiro/src/br/com/grupoferraz/financeiro/domain/Produtos.java
@@ -0,0 +1,42 @@
+package br.com.grupoferraz.financeiro.domain;
+
+public class Produtos {
+	private long codigo;
+	private String descricao;
+	private long quantidade;
+	private double preco;
+	private Fornecedores fornecedores;
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public long getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(long quantidade) {
+		this.quantidade = quantidade;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public Fornecedores getFornecedores() {
+		return fornecedores;
+	}
+	public void setFornecedores(Fornecedores fornecedores) {
+		this.fornecedores = fornecedores;
+	}
+	
+	
+}
---
+++ financeiro/src/br/com/grupoferraz/financeiro/factory/ConexaoBD.java
@@ -0,0 +1,43 @@
+package br.com.grupoferraz.financeiro.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBD {
+	private static final String usuario = "root";
+	private static final String senha = "1234567";
+	private static final String url = "jdbc:mysql://localhost:3306/bancot";
+	
+	public static Connection conexao;
+	
+	public static Connection getConexao() {
+		if(conexao == null) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				conexao = DriverManager.getConnection(url,usuario,senha);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return conexao;
+	}
+	
+	public static void fecharConexao () {
+		if(conexao != null) {
+			try {
+				conexao.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			conexao = null;
+		}
+	}
+}
